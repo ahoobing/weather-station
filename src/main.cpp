0ea4deb30957824114476d4f84b216f4845f3ccb
Andy <andyh@northendlabs.com>
2019-12-08 12:26:01 -0700

    Weather Shield is working.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@ Distributed as-is; no warranty is given.
 // using it).
 #include <SoftwareSerial.h> 
 #include <SparkFunESP8266WiFi.h>
+#include <Wire.h> //I2C needed for sensors
+#include "SparkFunMPL3115A2.h" //Pressure sensor - Search "SparkFun MPL3115" and install from Library Manager
+//#include "SparkFun_Si7021_Breakout_Library.h" //Humidity sensor - Search "SparkFun Si7021" and install from Library Manager
+
 
 //////////////////////////////
 // WiFi Network Definitions //
@@ -35,6 +39,17 @@ Distributed as-is; no warranty is given.
 // password of your WiFi network.
 const char mySSID[] = "hobie4";
 const char myPSK[] = "hoob13579";
+
+const byte WSPEED = 3;
+const byte RAIN = 2;
+const byte STAT1 = 7;
+const byte STAT2 = 8;
+
+// analog I/O pins
+const byte REFERENCE_3V3 = A3;
+const byte LIGHT = A1;
+const byte BATT = A2;
+const byte WDIR = A0;
 
 //////////////////////////////
 // ESP8266Server definition //
@@ -57,6 +72,89 @@ const String htmlHeader = "HTTP/1.1 200 OK\r\n"
 const String httpRequest = "GET / HTTP/1.1\r\n"
                            "Host: weather-server-hoob.herokuapp.com\r\n"
                            "Connection: close\r\n\r\n";
+
+
+//Global Variables
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+long lastSecond; //The millis counter to see when a second rolls by
+byte seconds; //When it hits 60, increase the current minute
+byte seconds_2m; //Keeps track of the "wind speed/dir avg" over last 2 minutes array of data
+byte minutes; //Keeps track of where we are in various arrays of data
+byte minutes_10m; //Keeps track of where we are in wind gust/dir over last 10 minutes array of data
+
+long lastWindCheck = 0;
+volatile long lastWindIRQ = 0;
+volatile byte windClicks = 0;
+
+//We need to keep track of the following variables:
+//Wind speed/dir each update (no storage)
+//Wind gust/dir over the day (no storage)
+//Wind speed/dir, avg over 2 minutes (store 1 per second)
+//Wind gust/dir over last 10 minutes (store 1 per minute)
+//Rain over the past hour (store 1 per minute)
+//Total rain over date (store one per day)
+
+byte windspdavg[120]; //120 bytes to keep track of 2 minute average
+
+#define WIND_DIR_AVG_SIZE 120
+int winddiravg[WIND_DIR_AVG_SIZE]; //120 ints to keep track of 2 minute average
+float windgust_10m[10]; //10 floats to keep track of 10 minute max
+int windgustdirection_10m[10]; //10 ints to keep track of 10 minute max
+volatile float rainHour[60]; //60 floating numbers to keep track of 60 minutes of rain
+
+//These are all the weather values that wunderground expects:
+int winddir = 0; // [0-360 instantaneous wind direction]
+float windspeedmph = 0; // [mph instantaneous wind speed]
+float windgustmph = 0; // [mph current wind gust, using software specific time period]
+int windgustdir = 0; // [0-360 using software specific time period]
+float windspdmph_avg2m = 0; // [mph 2 minute average wind speed mph]
+int winddir_avg2m = 0; // [0-360 2 minute average wind direction]
+float windgustmph_10m = 0; // [mph past 10 minutes wind gust mph ]
+int windgustdir_10m = 0; // [0-360 past 10 minutes wind gust direction]
+float humidity = 0; // [%]
+float tempf = 0; // [temperature F]
+float rainin = 0; // [rain inches over the past hour)] -- the accumulated rainfall in the past 60 min
+volatile float dailyrainin = 0; // [rain inches so far today in local time]
+//float baromin = 30.03;// [barom in] - It's hard to calculate baromin locally, do this in the agent
+float pressure = 0;
+//float dewptf; // [dewpoint F] - It's hard to calculate dewpoint locally, do this in the agent
+
+float batt_lvl = 11.8; //[analog value from 0 to 1023]
+float light_lvl = 455; //[analog value from 0 to 1023]
+
+// volatiles are subject to modification by IRQs
+volatile unsigned long raintime, rainlast, raininterval, rain;
+
+
+//Interrupt routines (these are called by the hardware interrupts, not by the main code)
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void rainIRQ()
+// Count rain gauge bucket tips as they occur
+// Activated by the magnet and reed switch in the rain gauge, attached to input D2
+{
+  Serial.println("rain irq");
+  raintime = millis(); // grab current time
+  raininterval = raintime - rainlast; // calculate interval between this and last event
+
+  if (raininterval > 10) // ignore switch-bounce glitches less than 10mS after initial edge
+  {
+    dailyrainin += 0.011; //Each dump is 0.011" of water
+    rainHour[minutes] += 0.011; //Increase this minute's amount of rain
+
+    rainlast = raintime; // set up for next event
+  }
+}
+
+void wspeedIRQ()
+// Activated by the magnet in the anemometer (2 ticks per rotation), attached to input D3
+{
+  Serial.println("wind speed irq");
+  if (millis() - lastWindIRQ > 10) // Ignore switch-bounce glitches less than 10ms (142MPH max reading) after the reed switch closes
+  {
+    lastWindIRQ = millis(); //Grab the current time
+    windClicks++; //There is 1.492MPH for each click per second.
+  }
+}
 
 
 // errorLoop prints an error code, then loops forever.
@@ -212,10 +310,26 @@ void setup()
   // and the network it's connected to.
   displayConnectInfo();
 
-  
-  clientDemo();
-  
-  
+  pinMode(STAT1, OUTPUT); //Status LED Blue
+
+  //TODO This causes the server requests to not work.
+ // pinMode(STAT2, OUTPUT); //Status LED Green
+
+  pinMode(WSPEED, INPUT_PULLUP); // input from wind meters windspeed sensor
+  pinMode(RAIN, INPUT_PULLUP); // input from wind meters rain gauge sensor
+
+  pinMode(REFERENCE_3V3, INPUT);
+  pinMode(LIGHT, INPUT);
+
+  seconds = 0;
+  lastSecond = millis();
+
+  // attach external interrupt pins to IRQ functions
+  attachInterrupt(0, rainIRQ, FALLING);
+  attachInterrupt(1, wspeedIRQ, FALLING);
+
+  // turn on interrupts
+  interrupts();
   
 }
 
